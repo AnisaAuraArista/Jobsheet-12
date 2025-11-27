5a2c3f91e028ab7628f49d2161098f3dcfeb1d36
PraktikumDaspro <email@ristapolinema03.com>
2025-11-27 10:39:35 +0700

    Modifikasi Tugas3

--- RekapPenjualanCafe04.java
+++ RekapPenjualanCafe04.java
@@ -1,77 +1,67 @@
 import java.util.Scanner;
 
 public class RekapPenjualanCafe04 {
-    static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
-
-    public static void inputData(int[][] data) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < data.length; i++) {
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Masukkan jumlah hari penjualan: ");
+        int jumlahHari = sc.nextInt();
+        sc.nextLine();
+
+        String[] menu = new String[jumlahMenu];
+        int[][] data = new int[jumlahMenu][jumlahHari];
+
+        System.out.println("\nMasukkan nama setiap menu:");
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < jumlahMenu; i++) {
             System.out.println("Input penjualan untuk menu: " + menu[i]);
-            for (int j = 0; j < data[0].length; j++) {
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.print("  Hari ke-" + (j + 1) + ": ");
                 data[i][j] = sc.nextInt();
             }
         }
-        sc.close();
-    }
 
-    public static void tampilkanTabel(int[][] data) {
         System.out.println("\n=== DATA PENJUALAN ===");
         System.out.print("Menu/Hari\t");
-        for (int h = 1; h <= 7; h++) System.out.print("H" + h + "\t");
+        for (int h = 1; h <= jumlahHari; h++) System.out.print("H" + h + "\t");
         System.out.println();
 
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(menu[i] + "\t");
-            for (int j = 0; j < data[0].length; j++) {
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print(menu[i] + "\t\t");
+            for (int j = 0; j < jumlahHari; j++) {
                 System.out.print(data[i][j] + "\t");
             }
             System.out.println();
         }
-    }
 
-    public static void tampilkanTertinggi(int[][] data) {
         int maxTotal = 0;
-        int indeksMenu = 0;
+        int index = 0;
 
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < jumlahMenu; i++) {
             int total = 0;
-            for (int j = 0; j < data[0].length; j++) {
-                total += data[i][j];
-            }
+            for (int j = 0; j < jumlahHari; j++) total += data[i][j];
+
             if (total > maxTotal) {
                 maxTotal = total;
-                indeksMenu = i;
+                index = i;
             }
         }
 
-        System.out.println("\nMenu dengan total penjualan tertinggi:");
-        System.out.println(menu[indeksMenu] + " = " + maxTotal);
-    }
-
-    public static void tampilkanRataRata(int[][] data) {
-        System.out.println("\nRata-rata penjualan tiap menu:");
-
-        for (int i = 0; i < data.length; i++) {
-            int total = 0;
-
-            for (int j = 0; j < data[0].length; j++) {
-                total += data[i][j];
-            }
-
-            double rata = (double) total / data[0].length;
-            System.out.println(menu[i] + " = " + rata);
+        System.out.println("\nMenu penjualan tertinggi: " + menu[index] + " = " + maxTotal);
+        System.out.println("\nRata-rata tiap menu:");
+        for (int i = 0; i < jumlahMenu; i++) {
+            int tot = 0;
+            for (int j = 0; j < jumlahHari; j++) tot += data[i][j];
+            System.out.println(menu[i] + " = " + ((double) tot / jumlahHari));
         }
-    }
-
-    public static void main(String[] args) {
-        int[][] penjualan = new int[5][7];
-
-        inputData(penjualan);
-        tampilkanTabel(penjualan);
-        tampilkanTertinggi(penjualan);
-        tampilkanRataRata(penjualan);
+        sc.close(); 
     }
 }
 
